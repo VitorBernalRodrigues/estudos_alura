89ccabc160166ab09e272163b9bcb55e00a001b2
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-25 22:34:21 -0400

    terceiro exercicio

--- Carreira Node.js/cursopratico-laços/tarefa.js
+++ Carreira Node.js/cursopratico-laços/tarefa.js
@@ -20,3 +20,17 @@ for(let i = 10; i >= 0; i--) {
     }
 }
 
+
+// Você está criando um sistema de análise para um cliente que precisa identificar todos os números pares dentro de um intervalo informado. Ele quer saber quais valores são múltiplos de 2, para aplicar um filtro em dados financeiros.
+
+// Crie um programa que exiba todos os números pares de 1 até o número final fornecido pelo usuário.
+
+const ultimoNumero = 10
+
+
+
+for (let i = 1; i <= ultimoNumero; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
