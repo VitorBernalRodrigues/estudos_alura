45cbd1a6ebc33b8cd6c8b1f60c0ad452902e3d16
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-25 22:42:02 -0400

    quinta tarefa

--- Carreira Node.js/cursopratico-laços/tarefa.js
+++ Carreira Node.js/cursopratico-laços/tarefa.js
@@ -45,3 +45,19 @@ senhaCadastro = 'seguranç@2025'
 for(let i = 0; i < senhaCadastro.length; i++) {
     console.log(senhaCadastro[i])
 }
+
+
+// Você está criando um sistema de cadastro em que a pessoa usuária pode cadastrar quantos nomes quiser, um por vez. O processo deve continuar até que seja encontrado a palavra "fim" (com letras minúsculas).
+
+// Crie um programa que com base em uma entrada pré-definida de nomes, exiba cada um deles, e encerre somente quando encontrar o valor "fim".
+
+
+const entradas = ["Ana", "Bruno", "Carla", "fim", "Daniel"];
+
+for (let i = 0; i < entradas.length; i++) {
+    if (entradas[i] === 'fim') {
+        console.log('fim')
+        break
+    }
+    console.log(`nome: ${entradas[i]}`)
+}
