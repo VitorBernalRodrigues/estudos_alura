39b29ef7d946fb2b6202061cedbd8ed5b6d61f7a
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-27 15:50:40 -0400

    pratica 4 e 5

--- Carreira Node.js/cursopratico-fuction/exercicios.js
+++ Carreira Node.js/cursopratico-fuction/exercicios.js
@@ -20,3 +20,39 @@ const calculaJuros = (valor, juros, tempo) => {
 
 
 console.log(calculaJuros(1500, 5, 1));
+
+// r iar uma função que faça operações matemáticas entre 2 valores (soma e multiplicação)
+// função deve receber por parâmetro: operação desejada, valor 1 e valor 2
+
+const soma = (a, b) => {
+    return a + b
+}
+
+const multiplicar = (a, b) => {
+    return a * b
+}
+
+const operacaoMatematica = (fnOperacao, valorA, valorB) => {
+    return fnOperacao(valorA, valorB)
+}
+
+console.log(operacaoMatematica(soma, 4, 2))
+console.log(operacaoMatematica(multiplicar, 5, 10))
+
+// const usuarioid = '241234'
+
+// function tempoTela(tempo)  {
+//     if(usuarioid === '241234') {
+//         setTimeout(() => {
+//             return console.log('tempo limite atingido')
+//         }, tempo)
+//     }     
+// }
+
+// tempoTela(2000)
+
+
+const userId = '4545656'
+
+setTimeout((userId) => console.log(`sessão de ${userId} está inativa`), 4000, userId);
+
