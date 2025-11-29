28e0f2e6c5d867405c76f416970573c626b6ba27
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-29 19:39:48 -0400

    tarefa 6

--- Carreira Node.js/cursopratico-fuction/tarefas.js
+++ Carreira Node.js/cursopratico-fuction/tarefas.js
@@ -111,3 +111,23 @@ function mensagemPersonalizada(nome, tipo) {
 }
  
 processarPedido("Lucas", "vip", mensagemPersonalizada)
+
+// Imagine que você está desenvolvendo uma interface de atendimento online.Quando o usuário envia uma pergunta, o sistema precisa simular que está "pensando" ou "processando a resposta", e só depois exibir a resposta final.
+
+// Sua tarefa é criar uma função chamada responderUsuario, que receba dois parâmetros:
+
+// o nome do usuário, e
+// uma função de callback que será executada após 3 segundos.
+
+function responderUsuario(nome, callback) {
+    console.log("Processando sua pergunta...")
+    setTimeout(() => {
+        callback(nome)
+    }, 3000)
+}
+ 
+function mostrarResposta(nome) {
+    console.log(`Olá, ${nome}! Aqui está a resposta para sua dúvida.`)
+}
+ 
+responderUsuario("Camila", mostrarResposta)
