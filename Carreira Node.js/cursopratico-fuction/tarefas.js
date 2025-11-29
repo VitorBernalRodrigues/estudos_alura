9bd6f1de0cd4d08f26f00e0131d97b0ee9deb668
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-29 18:54:32 -0400

    tarefa 3

--- Carreira Node.js/cursopratico-fuction/tarefas.js
+++ Carreira Node.js/cursopratico-fuction/tarefas.js
@@ -34,3 +34,13 @@ function calcularDesconto(precoOriginal, porcentagem) {
 }
 
 calcularDesconto(100, );
+
+
+// Imagine que você está criando uma pequena ferramenta para alunos acompanharem seu desempenho escolar. A ideia é que o aluno insira suas duas notas principais e o sistema exiba a média aritmética entre elas.
+
+// Sua tarefa é criar uma arrow function chamada calcularMedia que receba duas notas e retorne a média entre elas.
+
+const calcularMedia = (a, b) => {return console.log((a + b) / 2)}
+
+
+calcularMedia(7, 9);
