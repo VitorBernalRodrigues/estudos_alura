d375694a4aff2fb1f2ef96a5bbdef4ef4a408908
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-29 18:42:11 -0400

    tarefas 1 e 2

---
+++ Carreira Node.js/cursopratico-fuction/tarefas.js
@@ -0,0 +1,36 @@
+// Imagine que você está criando uma página de boas-vindas para um site de cursos online. Ao acessar a plataforma, o sistema precisa exibir uma mensagem de saudação para cada usuário. Às vezes, porém, o nome da pessoa pode estar ausente — e, nesse caso, uma saudação genérica deve ser exibida.
+
+// Sua tarefa é criar uma função chamada saudacao que receba um nome como parâmetro, mas que funcione mesmo quando nenhum nome for informado, exibindo uma saudação genérica. Use o parâmetro pré-definido para lidar com isso.
+
+function saudacao(nome) {
+    if(nome === undefined) {
+        return console.log('Olá, Seja bem vindo!!!')
+    } else {
+        return console.log(`Olá ${nome}, Seja bem vindo!!!`)
+    } 
+}
+
+saudacao()
+saudacao('Vitor')
+
+
+// Imagine que você está criando um sistema para um e-commerce que vende livros.Toda vez que o cliente aplica um cupom de desconto, o sistema precisa calcular quanto ele vai pagar com base no valor original e na porcentagem de desconto.
+
+// Sua missão é criar uma função chamada calcularDesconto que receba dois parâmetros:
+
+// o preço original do produto,
+// a porcentagem de desconto (com um valor padrão de 10%).
+
+function calcularDesconto(precoOriginal, porcentagem) {
+    if (porcentagem === undefined || 0) {
+        let porcentagem = 10
+        let precofinal = precoOriginal * (1 - porcentagem / 100)
+        return console.log(`Preço final com desconto de ${porcentagem}% é: R$${precofinal}`)
+    } else {
+        let precofinal = precoOriginal * (1 - porcentagem / 100)
+        return console.log(`Preço final com desconto de ${porcentagem}% é: R$${precofinal}`)
+    }
+
+}
+
+calcularDesconto(100, );
