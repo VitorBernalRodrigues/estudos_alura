94cf99f8c773483cd1107c57cc9c9ed5aa864b21
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-29 19:43:26 -0400

    tarefa 10

--- Carreira Node.js/cursopratico-fuction/tarefas.js
+++ Carreira Node.js/cursopratico-fuction/tarefas.js
@@ -252,3 +252,23 @@ const participantes = [
 ];
  
 realizarSorteio(participantes);
+
+
+// Sua missão é criar uma função chamada contagemRegressiva que:
+
+// Receba um número inteiro positivo (ex: 5)
+// Imprima esse número e, a cada chamada, reduza em 1
+// Quando chegar em 0, exiba a mensagem: "Lançamento!"
+// A função deve ser implementada de forma recursiva, sem usar loops (for ou while).
+
+
+function contagemRegressiva(numero) {
+  if (numero > 0) {
+    console.log(numero)
+    contagemRegressiva(numero - 1)
+  } else {
+    console.log("Lançamento!")
+  }
+}
+ 
+contagemRegressiva(5)
