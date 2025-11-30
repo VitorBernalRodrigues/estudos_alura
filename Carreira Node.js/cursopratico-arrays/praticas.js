539acdde2c5898476fd94ca027cbaa8f74413b7d
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-30 14:07:20 -0400

    pratica 2

--- Carreira Node.js/cursopratico-arrays/praticas.js
+++ Carreira Node.js/cursopratico-arrays/praticas.js
@@ -1,3 +1,4 @@
+// 1
 const arr = [0, 2, 4, 10]
 
 console.log(arr)
@@ -6,3 +7,19 @@ console.log(arr[3])
 arr[4] = 20
 console.log(arr)
 
+// 2
+
+// código omitido
+
+function criaArrNum(inicio, fim) {
+  const arr = []
+  let elemento = inicio
+
+  for(let i = 0, j = 0; j < fim; i++, elemento++) {
+    arr[i] = elemento
+    j = elemento
+  }
+  return arr
+}
+
+console.log(criaArrNum(40, 70))
