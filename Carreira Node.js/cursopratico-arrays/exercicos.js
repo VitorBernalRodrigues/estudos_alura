a9a9adff6cae3b1e3dcca7cd41f0534140f66d9a
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-30 17:45:16 -0400

    exercicio 2

--- Carreira Node.js/cursopratico-arrays/exercicos.js
+++ Carreira Node.js/cursopratico-arrays/exercicos.js
@@ -12,3 +12,20 @@ const listaDeFrutasCompras = ['Tomate', 'Maçã', 'Banana', 'Manga']
 console.log('Segundo item da lista:', listaDeFrutasCompras[1])
 listaDeFrutasCompras[3] = 'Uva'
 console.log(listaDeFrutasCompras)
+
+
+// Você está criando um sistema para controlar os gastos mensais da sua casa. Os valores das despesas estão em um array, e você precisa somar todos esses valores para saber o total gasto no mês.
+
+// Escreva um programa que:
+
+// Crie um array com os valores das despesas.
+// Use um loop for para somar os valores.
+// Exiba o total gasto.
+
+const despesas = [120, 80, 45.5, 200, 60]
+let soma = 0;
+for (let i = 0; i < despesas.length; i++) {
+    soma += despesas[i]
+}
+
+console.log('Total de despesas: R$', soma)
