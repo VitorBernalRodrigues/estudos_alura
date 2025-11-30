012e577abdfe5d9fbe7e86dd9f487308d3a78359
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-30 17:38:12 -0400

    exercicio 1

---
+++ Carreira Node.js/cursopratico-arrays/exercicos.js
@@ -0,0 +1,14 @@
+// Imagine que você está desenvolvendo um sistema para controlar uma lista de compras. O primeiro passo é permitir que a pessoa usuária acesse e altere os itens dessa lista.
+
+// Escreva um programa que:
+
+// Crie um array com os itens iniciais da lista de compras.
+// Acesse e exiba o segundo item da lista.
+// Modifique o último item da lista para um novo valor.
+
+
+const listaDeFrutasCompras = ['Tomate', 'Maçã', 'Banana', 'Manga']
+
+console.log('Segundo item da lista:', listaDeFrutasCompras[1])
+listaDeFrutasCompras[3] = 'Uva'
+console.log(listaDeFrutasCompras)
