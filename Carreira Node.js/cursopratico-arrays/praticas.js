49fbbcb8fb9738a25e782121b21f6f0f8570d142
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-30 14:46:38 -0400

    pratica 3

--- Carreira Node.js/cursopratico-arrays/praticas.js
+++ Carreira Node.js/cursopratico-arrays/praticas.js
@@ -23,3 +23,40 @@ function criaArrNum(inicio, fim) {
 }
 
 console.log(criaArrNum(40, 70))
+
+// 3
+
+const arr1 = [[1, 2], 2, 3];
+
+const copiaArray = (arr) => {
+  const copia = [];
+  arr.forEach((elem) => {
+    if (Array.isArray(elem)) {
+      copia.push(copiaArray(elem));
+    } else {
+      copia.push(elem);
+    }
+  })
+  return copia;
+}
+
+const arr2 = copiaArray(arr1);
+
+
+// código omitido
+
+// filtrar um array e alterar valores específicos
+// ex: alterar números para strings
+
+const cpfs = ['12323434545', '34658756786', 43254365434, '76547865345', 56768767564]
+
+const cpfsValidos = cpfs.map((cpf) => {
+    if(typeof cpf === 'string') {
+        return cpf
+    } else {
+        return cpf.toString()
+    }
+})
+
+console.log(cpfsValidos)
+
