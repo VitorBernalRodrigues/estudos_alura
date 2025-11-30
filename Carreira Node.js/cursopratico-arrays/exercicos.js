5d43b3ee1828855615b4b5219ebb5a91c93c5dc6
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-30 17:53:28 -0400

    exercicio 4

--- Carreira Node.js/cursopratico-arrays/exercicos.js
+++ Carreira Node.js/cursopratico-arrays/exercicos.js
@@ -41,3 +41,18 @@ const estudantes = ['Carla', 'João', 'Marina', 'Lucas', 'Beatriz']
 for (const name of estudantes) {
     console.log(`estudante: ${name}`)
 }
+
+// Você recebeu a missão de desenvolver um sistema de envio de notificações. A cada nova mensagem cadastrada, o sistema deve exibir uma notificação no console informando que a mensagem foi enviada. Escreva um programa que:
+
+// Crie um array com uma lista de mensagens.
+// Use o método forEach() para exibir uma mensagem no console para cada item da lista.
+
+const mensagens = ['Pedido confirmado', 'Pagamento aprovado', 'Produto enviado']
+
+mensagens.forEach((mensagem) => {
+    console.log(mensagem)
+})
+
+
+
+
