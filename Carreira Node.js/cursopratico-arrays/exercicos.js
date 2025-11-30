1c117084ebed0bbacd2ddfee53db9ae7e5baae3b
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-11-30 17:48:43 -0400

    exercicio 3

--- Carreira Node.js/cursopratico-arrays/exercicos.js
+++ Carreira Node.js/cursopratico-arrays/exercicos.js
@@ -29,3 +29,15 @@ for (let i = 0; i < despesas.length; i++) {
 }
 
 console.log('Total de despesas: R$', soma)
+
+// Você está criando um painel para um sistema de chamada escolar. Você recebeu uma lista com os nomes das pessoas estudantes de uma turma e precisa exibir um por um para fazer a chamada em sala. Escreva um programa que:
+
+// Crie um array com os nomes das pessoas.
+// Use o for...of para exibir cada nome individualmente.\
+
+
+const estudantes = ['Carla', 'João', 'Marina', 'Lucas', 'Beatriz']
+
+for (const name of estudantes) {
+    console.log(`estudante: ${name}`)
+}
