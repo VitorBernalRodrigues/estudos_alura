5a15823957c6656e8b26063187a4f56a6dadfb26
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-02 19:25:27 -0400

    exercicio 7

--- Carreira Node.js/cursopratico-arrays/exercicos.js
+++ Carreira Node.js/cursopratico-arrays/exercicos.js
@@ -83,3 +83,20 @@ pedidosCopia.push('boné')
 
 console.log("Array original:", pedidos)
 console.log("Array copiado:", pedidosCopia)
+
+
+// Você está desenvolvendo um sistema de busca para uma biblioteca digital. Quando a pessoa usuária digita o nome de um livro, o sistema precisa verificar se ele está disponível na lista de títulos cadastrados. Escreva um programa que:
+
+// Crie um array com nomes de livros.
+// Crie uma variável com o nome de um livro procurado.
+// Verifique se o livro está na lista.
+// Exiba uma mensagem informando se o livro foi encontrado ou não.
+
+const livros = ['Dom Casmurro', 'O Cortiço', 'Capitães da Areia', 'Iracema']
+const livroProcurado = 'O Cortiço'
+
+if (livros.indexOf(livroProcurado) !== -1) {
+  console.log(`O livro "${livroProcurado}" está disponível.`);
+} else {
+  console.log(`O livro "${livroProcurado}" não foi encontrado.`);
+}
