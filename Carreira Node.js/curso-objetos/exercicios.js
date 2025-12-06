e73e0afe8e06dd20c9815db723f63c9b5bb388b9
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-06 16:32:36 -0400

    exercicio 2

--- Carreira Node.js/curso-objetos/exercicios.js
+++ Carreira Node.js/curso-objetos/exercicios.js
@@ -12,3 +12,22 @@ const produto = {
 
 console.log(produto)
 
+
+// Você está criando um painel de configurações para um aplicativo. As configurações são armazenadas em um objeto e você precisa acessar informações específicas para exibição.
+
+// Escreva um programa que:
+
+// Crie um objeto configuracoes com as propriedades de tema, notificacoes, idioma.
+// Acesse tema usando notação de ponto.
+// Acesse idioma usando notação de colchetes.
+// Exiba os dois valores no console.
+
+const configuracoes = {
+    tema : 'escuro',
+    notificacoes : 2,
+    idioma : ['Portugues BR', 'English', 'Espanol']
+}
+
+console.log(configuracoes.tema)
+console.log(configuracoes.idioma[0]);
+
