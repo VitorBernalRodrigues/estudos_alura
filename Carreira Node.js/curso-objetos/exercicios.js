d25b4fc1fb8cdfb78398b8f861c70bf69571b3e8
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-06 16:50:16 -0400

    exercicio 5

--- Carreira Node.js/curso-objetos/exercicios.js
+++ Carreira Node.js/curso-objetos/exercicios.js
@@ -74,3 +74,25 @@ const dispositivos = [
 for (const dispositivo of dispositivos) {
     console.log(`Dispositivo: ${dispositivo.nome} | Status: ${dispositivo.status}`)
 }
+
+
+// Você está desenvolvendo um sistema de controle de máquinas em uma fábrica. Cada máquina é representada por um objeto que, além das informações básicas, também possui um método interno que exibe seu status. Escreva um programa que:
+
+// Crie um objeto maquina com as propriedades: nome, funcionando.
+// Adicione um método chamado exibirStatus que exibe no console: "A máquina [nome] está em funcionamento." ou "A máquina [nome] está parada.", dependendo do valor de funcionando.
+// Chame o método exibirStatus.
+
+const maquina = {
+    nome: 'Esteira Transportadora',
+    funcionando: true,
+}
+
+maquina.exibirStatus = function exibirStatus() {
+    if(this.funcionando == true) {
+        console.log(`A máquina ${this.nome} está em funcionamento.`)
+    } else {
+        console.log(`A máquina ${this.nome} está parada.`)
+    }
+}
+
+maquina.exibirStatus();
