d137bceb6376bafd16d8f725be979998507a508e
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-06 16:34:47 -0400

    exercicio 3

--- Carreira Node.js/curso-objetos/exercicios.js
+++ Carreira Node.js/curso-objetos/exercicios.js
@@ -31,3 +31,21 @@ const configuracoes = {
 console.log(configuracoes.tema)
 console.log(configuracoes.idioma[0]);
 
+// Você está desenvolvendo um sistema para uma locadora de carros. Cada veículo tem informações cadastradas, mas agora a empresa decidiu remover a informação do modelo antigo dos carros. Escreva um programa que:
+
+// Crie um objeto carro com as propriedades: marca, modelo, ano, modeloAntigo;
+// Remova a propriedade modeloAntigo;
+// Exiba o objeto final no console.
+
+
+const carro = {
+  marca: 'Toyota',
+  modelo: 'Corolla',
+  ano: 2022,
+  modeloAntigo: 'Corolla XEi'
+}
+
+delete carro.modeloAntigo
+
+console.log(carro);
+
