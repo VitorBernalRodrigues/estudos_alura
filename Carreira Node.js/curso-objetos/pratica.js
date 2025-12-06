5500031a5d2aafe2a21d8ccd626047d6bfcfb72d
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-06 15:51:28 -0400

    pratica 2

--- Carreira Node.js/curso-objetos/pratica.js
+++ Carreira Node.js/curso-objetos/pratica.js
@@ -18,3 +18,22 @@ function exibirTrofeus(listaTrofeus) {
 }
 
 exibirTrofeus(pessoa.trofeus)
+
+
+// 2
+
+// adicione ao obj um método para calcular a idade da pessoa e retornar o valor no terminal como string.
+
+const user = {
+    nome: 'Roberta R',
+    nascimento: '1996-01-01',
+    cpf: '23445667889',
+    pontuacao: 4576,
+    trofeus: ['speedrunner', 'indie']
+};
+
+user.calcularIdade = function calcularIdade() {
+    const nascimento = parseInt(this.nascimento.slice(0, 4))
+    const idade = new Date().getFullYear() - nascimento
+    return console.log(idade)
+}
