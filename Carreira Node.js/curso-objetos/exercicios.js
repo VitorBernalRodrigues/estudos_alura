468a9223b9dab2d5db7ff278bc59c3eb63a97e41
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-06 16:28:28 -0400

    exercicio 1

---
+++ Carreira Node.js/curso-objetos/exercicios.js
@@ -0,0 +1,14 @@
+// Você está desenvolvendo um sistema para uma loja online. Cada produto do catálogo deve ser representado por um objeto com informações de nome, preco e disponivel. Escreva um programa que:
+
+// Crie um objeto chamado produto.
+// O objeto deve conter as propriedades: nome, preco, disponivel.
+// Exiba o objeto no console.
+
+const produto = {
+    nome : 'copo',
+    preco : 'R$1.99',
+    disponivel : 'true'
+}
+
+console.log(produto)
+
