f14dbd23bca9aeb870f242d5e9ea0820d7fc3320
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-07 13:58:54 -0400

    exercicio 8

--- Carreira Node.js/curso-objetos/exercicios.js
+++ Carreira Node.js/curso-objetos/exercicios.js
@@ -121,3 +121,33 @@ const dados = [
 ]
  
 console.log(montarObjeto(dados))
+
+
+// Você foi a pessoa encarregada de desenvolver uma funcionalidade para o sistema de inspeções técnicas de uma fábrica. Cada máquina inspecionada gera automaticamente um relatório de medições, contendo dados como temperatura, vibração, pressão e nível de ruído. Essas informações são organizadas dentro de um objeto, onde cada chave representa uma categoria avaliada e o valor representa o nível registrado na inspeção.
+
+// Exibir todas as categorias avaliadas;
+// Exibir todos os valores registrados;
+// Percorra todas as medições (pares categoria/valor) e imprima um detalhamento com status, seguindo a seguinte lógica:
+// Se o valor for maior que 50, mostre a mensagem (alerta)
+// Caso contrário, mostre (ok)
+
+const relatorio = {
+  temperatura: 75,
+  vibracao: 40,
+  pressao: 55,
+  nivelRuido: 30
+}
+
+console.log(`categorias validas: `, Object.keys(relatorio))
+
+console.log('Valores registrados', Object.values(relatorio))
+
+const entradas = Object.entries(relatorio)
+
+console.log("\nDetalhamento:")
+
+for (let [chave, valor] of entradas) {
+  const status = valor > 50 ? "alerta" : "ok"
+  console.log(`${chave}: ${valor} (${status})`)
+}
+
