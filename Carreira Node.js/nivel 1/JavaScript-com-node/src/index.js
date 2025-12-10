f11997c26281db7896fb5c5c049ff5face025d32
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-09 20:49:01 -0400

    aula dois

--- Carreira Node.js/nivel 1/JavaScript-com-node/src/index.js
+++ Carreira Node.js/nivel 1/JavaScript-com-node/src/index.js
@@ -1,30 +1,44 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const caminhoArquivo = process.argv
-const link = caminhoArquivo[2]
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
 
 if (!link) {
-    console.error('Informe o caminho do arquivo como argumento.')
-    process.exit(1)
+    console.error("Informe o caminho do arquivo como argumento.");
+    process.exit(1);
 }
 
-fs.readFile(link, 'utf-8', (err, texto) => {
+fs.readFile(link, "utf-8", (err, texto) => {
     if (err) {
-        console.error('Erro ao ler o arquivo:', err.message)
-        return
+        console.error("Erro ao ler o arquivo:", err.message);
+        return;
     }
-    const textoMinusculo = texto.toLowerCase()
-    verificarPalavrasDuplicadas(textoMinusculo)
-})
+    quebraEmParagrafos(texto);
+});
 
-// console.log(link)
-
-function verificarPalavrasDuplicadas(texto) {
-    const listaPalavras = texto.split(' ')
-    const resultado = {}
-    listaPalavras.forEach(palavra => {
-        if (!palavra) return
-        resultado[palavra] = (resultado[palavra] || 0) + 1
-        console.log(resultado)
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if (!paragrafo) return [];
+        return verificaPalavrasDuplicadas(paragrafo);
     })
+    console.log(contagem);
+}
+
+
+function limpaPalavra(texto) {
+    return texto.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(" ");
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach((palavra) => {
+        if (palavra.length >= 3) {
+            const palavraLimpa = limpaPalavra(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+        }
+
+    });
+    return resultado;
 }
