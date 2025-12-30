ff423e98f8d24be8322ff6cbbc3c0e1529bcf6cf
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-12-30 19:49:48 -0400

    rotas
    tratamentos de rotas com postgress

--- Carreira Node.js/nivel 1/criando_api/server.js
+++ Carreira Node.js/nivel 1/criando_api/server.js
@@ -1,21 +1,17 @@
-import http from 'http';
+// import http from 'http';
+import app from './src/app.js';
 // import { routes } from './routes.js';
 
 const PORT = 3000;
 
-const rotas = {
-    '/': 'Curso de Node.js na Alura!',
-    '/livros': 'Você está na página de livros',
-    '/autores': 'Você está na página de autores',
-    // '/editoras': 'Você está na página de editoras'
-};
 
-const server = http.createServer((req, res) => {
-    // const { method, url } = req;
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-    res.end(rotas[req.url]);
-})
 
-server.listen(PORT, () => {
+// const server = http.createServer((req, res) => {
+//     // const { method, url } = req;
+//     res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+//     res.end(rotas[req.url]);
+// })
+
+app.listen(PORT, () => {
     console.log('Servidor rodando na porta 3000');
 });
