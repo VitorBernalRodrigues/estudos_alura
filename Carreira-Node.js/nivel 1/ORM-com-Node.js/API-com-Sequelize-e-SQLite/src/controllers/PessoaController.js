7ed2638814adec31f5942846d6e3dd5177459644
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2026-05-02 14:36:00 -0400

    database

--- Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/controllers/PessoaController.js
+++ Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/controllers/PessoaController.js
@@ -1,11 +1,10 @@
+const database = require('../models');
+
 class PessoaController {
     static async pegaTodasAsPessoas(req, res) {
         try {
-            const pessoas = [
-                { id: 1, nome: 'João', idade: 30 },
-                { id: 2, nome: 'Maria', idade: 25 },
-            ];
-            res.status(200).json(pessoas);
+            const listasPessoas = await database.Pessoa.findAll();
+            res.status(200).json(listasPessoas);
         } catch (error) {
             res.status(500).json({ error: 'Erro ao pegar pessoas' });
         }
@@ -14,12 +13,11 @@ class PessoaController {
     static async createPessoa(req, res) {
         try {
             const { nome, idade } = req.body;
-            // Lógica para criar uma nova pessoa (exemplo usando um modelo fictício)
-            const novaPessoa = { id: Date.now(), nome, idade };
-            // Aqui você pode salvar a nova pessoa em um banco de dados
+            const novaPessoa = await database.Pessoa.create({ nome, idade });
             res.status(201).json(novaPessoa);
         } catch (error) {
             res.status(500).json({ error: 'Erro ao criar pessoa' });
         }
     }   
 }
+        
