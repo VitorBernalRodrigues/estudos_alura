168d8015b9ee4373fbf4b98732f31b607a72d2c8
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2026-05-09 15:06:16 -0400

    validacaocpf

--- Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/database/models/pessoa.js
+++ Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/database/models/pessoa.js
@@ -1,33 +1,54 @@
 'use strict';
+const isCpfValido = require('../utils/validaCpfHelper');
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
-    
+
     static associate(models) {
       Pessoa.hasMany(models.Curso, { foreignKey: 'docente_id', scope: { status: 'matriculado' }, as: 'aulasMatriculadas' });
       Pessoa.hasMany(models.Matricula, { foreignKey: 'estudante_id' });
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'O campo nome deve conter entre 3 e 30 caracteres.'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'O campo email deve conter um endereço de email válido.'
+        }
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if(!isCpfValido(cpf)) throw new Error('O campo CPF deve conter um número válido.');
+        }
+      }
+    }
+  }),
+  {
     sequelize,
     modelName: 'Pessoa',
     tableName: 'pessoas',
     paranoid: true,
     defaultScope: {
       where: {
-        status: 'ativo'
+        ativo: true
       }
     },
-    scopes: {
-      todosOsRegistros: {
-        where: {}
-      }
-    }
-  });
-  return Pessoa;
-};
+  }
+}
+    
