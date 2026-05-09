6733fa1f8cd6b48cb31546b3f5be61229103a16c
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2026-05-09 15:39:12 -0400

    adicionando escopos de associacao

--- Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/controllers/PessoaController.js
+++ Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/controllers/PessoaController.js
@@ -8,10 +8,21 @@ class PessoaController extends Controller {
         super(pessoaServices);
     }       
 
-    async pegaTodasAsPessoas(req, res) {
+    async pegaMatriculaAtivas(req, res) {
         const { estudanteId } = req.params;
         try {
-            const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+            const listaMatriculas = await pessoaServices.pegaMatriculasAtivasPorEstudante(Number(estudanteId));
+            return res.status(200).json(listaMatriculas);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    async pegaTodasAsMatriculas(req, res) {
+        const { estudanteId } = req.params;
+        try {
+            const estudante = await pessoaServices.pegaTodasAsMatriculasPorEstudante(Number(estudanteId));
+            const listaMatriculas = await estudante.getTodasAsMatriculadas();
             return res.status(200).json(listaMatriculas);
         } catch (error) {
             return res.status(500).json(error.message);
--- Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/database/models/pessoa.js
+++ Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/database/models/pessoa.js
@@ -7,8 +7,15 @@ module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
 
     static associate(models) {
-      Pessoa.hasMany(models.Curso, { foreignKey: 'docente_id', scope: { status: 'matriculado' }, as: 'aulasMatriculadas' });
-      Pessoa.hasMany(models.Matricula, { foreignKey: 'estudante_id' });
+      Pessoa.hasMany(models.Curso, { foreignKey: 'docente_id' });
+      Pessoa.hasMany(models.Matricula, { 
+        foreignKey: 'estudante_id', 
+        scope:{status: 'matriculado'}, as: 'aulasMatriculadas' 
+      });
+      Pessoa.hasMany(models.Matricula, { 
+        foreignKey: 'estudante_id', 
+        as: 'todasAsMatriculadas' 
+      });
     }
   }
   Pessoa.init({
