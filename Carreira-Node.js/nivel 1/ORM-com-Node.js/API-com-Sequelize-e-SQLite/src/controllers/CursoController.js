756b3453976d308d5c0f7f757f4d8929d3d91c49
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2026-05-09 16:50:49 -0400

    trabalhando com operadores

--- Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/controllers/CursoController.js
+++ Carreira-Node.js/nivel 1/ORM-com-Node.js/API-com-Sequelize-e-SQLite/src/controllers/CursoController.js
@@ -6,7 +6,17 @@ const cursoServices = new CursoServices();
 class CursoController extends Controller {
     constructor() {
         super(cursoServices);
-    }       
+    }
+
+    async pegaCursos(req, res) {
+        const { data_inicial, data_final } = req.query;
+
+        const where = {};
+
+        data_inicial || data_final ? where.data_inicio = {} : null;
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+        data_final ? where.data_inicio[Op.lte] = data_final : null;
+    }
 }
 
 module.exports = new CursoController();
