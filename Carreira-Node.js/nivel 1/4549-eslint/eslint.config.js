c03062e1cb3eec6c8651eb0e6dc880a471831e3f
Vitor Hugo Rodrigues <vitorhugobernal@gmail.com>
2026-05-14 17:19:06 -0400

    mais config eslint

--- Carreira-Node.js/nivel 1/4549-eslint/eslint.config.js
+++ Carreira-Node.js/nivel 1/4549-eslint/eslint.config.js
@@ -1,15 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import { plugin } from "mongoose";
+import plugins from "eslint-plugin-import";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }, 
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, import: plugins }, extends: ["js/recommended"], languageOptions: { globals: globals.node }, 
     rules: {
       "semi": ["error", "always"],
       "quotes": ["error", "double"],
       "indent": ["error", 4],
       "no-trailing-spaces": ["error"],
       "prefer-const": ["error"],
+      "no-param-reassign": ["error"],
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "array-bracket-spacing": ["error", "never"],
+      "comma-dangle": ["error", "always-multiline"],
+      "camelcase": ["error"],
+      "no-var": ["error"],
+      "import/no-cycle": ["error"],
     },
   },
 ]);
